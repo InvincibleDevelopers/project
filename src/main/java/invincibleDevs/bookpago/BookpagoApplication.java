bd0e5fc19dd6686afee394449741883df8fa81b1
pogeum <qha3012@gmail.com>
2024-09-07 13:38:14 +0900

    firstPush

---
+++ src/main/java/invincibleDevs/bookpago/BookpagoApplication.java
@@ -0,0 +1,13 @@
+package invincibleDevs.bookpago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookpagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookpagoApplication.class, args);
+	}
+
+}
