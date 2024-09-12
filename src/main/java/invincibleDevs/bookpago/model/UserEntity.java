525a68b8e57ec376e2213e7b673647f589cb6be5
Airysm <shwon775@naver.com>
2024-09-12 14:02:44 +0900

    #7 - UserEntity 생성
    JPA로 Database에 UserEntity table을 만들었다.

---
+++ src/main/java/invincibleDevs/bookpago/model/UserEntity.java
@@ -0,0 +1,22 @@
+package invincibleDevs.bookpago.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role;
+}
