243e3994e0425a0693e60531e722d0c6fcb3afbe
Airysm <shwon775@naver.com>
2024-09-12 14:40:51 +0900

    #7 - 테스트용 컨트롤러
    Postman으로 테스트를 하기 위해 Main과 Admin Controller을 만들었습니다.

---
+++ src/main/java/invincibleDevs/bookpago/controller/MainController.java
@@ -0,0 +1,15 @@
+package invincibleDevs.bookpago.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ResponseBody
+public class MainController {
+    @GetMapping("/")
+    public String mainP() {
+
+        return "Main Controller";
+    }
+}
