fc58d86c58bac09d744f9432c2b83b0cc98410e4
Airysm <shwon775@naver.com>
2024-09-12 14:15:26 +0900

    #7 - 회원가입
    아이디, 비밀번호를 받아 DB에 저장한다.

---
+++ src/main/java/invincibleDevs/bookpago/controller/JoinController.java
@@ -0,0 +1,22 @@
+package invincibleDevs.bookpago.controller;
+
+import invincibleDevs.bookpago.dto.JoinDTO;
+import invincibleDevs.bookpago.service.JoinService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@ResponseBody
+public class JoinController {
+    private final JoinService joinService;
+
+    @PostMapping("/join")
+    public String joinProcess(JoinDTO joinDTO) {
+        joinService.joinProcess(joinDTO);
+
+        return "ok";
+    }
+}
