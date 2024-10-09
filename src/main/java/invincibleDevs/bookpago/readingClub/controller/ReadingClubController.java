de21c7470a5f7c9e8a53e615b5487dca7d26e226
pogeum <qha3012@gmail.com>
2024-10-09 18:59:41 +0900

    최근저장

--- src/main/java/invincibleDevs/bookpago/readingClub/controller/ReadingClubController.java
+++ src/main/java/invincibleDevs/bookpago/readingClub/controller/ReadingClubController.java
@@ -6,7 +6,14 @@ import invincibleDevs.bookpago.readingClub.service.ReadingClubFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,6 +39,7 @@ public class ReadingClubController {
             @RequestBody ReadingClubRequest readingClubRequest
     ) {
         try {
+            System.out.println(readingClubRequest);
             return ResponseEntity.ok(readingClubFacade.createClub(readingClubRequest));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
@@ -49,7 +57,7 @@ public class ReadingClubController {
     public ResponseEntity<?> joinClub(
             @PathVariable("clubId") Long clubId,
             @RequestBody ReadingClubMapRequest readingClubMapRequest
-            ) {
+    ) {
         try {
             return ResponseEntity.ok(readingClubFacade.joinClub(clubId, readingClubMapRequest));
         } catch (Exception e) {
@@ -87,7 +95,8 @@ public class ReadingClubController {
             @RequestBody ReadingClubMapRequest readingClubMapRequest
     ) {
         try {
-            return ResponseEntity.ok(readingClubFacade.rejectApplicants(clubId, readingClubMapRequest));
+            return ResponseEntity.ok(
+                    readingClubFacade.rejectApplicants(clubId, readingClubMapRequest));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
@@ -99,7 +108,8 @@ public class ReadingClubController {
             @RequestBody ReadingClubMapRequest readingClubMapRequest
     ) {
         try {
-            return ResponseEntity.ok(readingClubFacade.acceptApplicants(clubId, readingClubMapRequest));
+            return ResponseEntity.ok(
+                    readingClubFacade.acceptApplicants(clubId, readingClubMapRequest));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
